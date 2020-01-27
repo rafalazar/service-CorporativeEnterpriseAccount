d290911fc96c60ddc63349e642eaa780273603dc
Jordan <jcr.salazar@outlook.com>
2020-01-27 10:43:09 -0500

    Controller added.

---
+++ src/main/java/com/rafalazar/bootcamp/app/controller/CorporativeEnterpriseController.java
@@ -0,0 +1,12 @@
+package com.rafalazar.bootcamp.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/corportativeEnterprise")
+public class CorporativeEnterpriseController {
+	
+	
+
+}
